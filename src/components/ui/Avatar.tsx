7c36f63b0275f4216e7f5a45010647a8443c30ab
rasheed <demandwork247@gmail.com>
2025-01-10 05:23:18 +0100

    Refactor imports in UI components and update Tailwind configuration for consistency

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 
 const Avatar = lazy(() =>
-  import("@nextui-org/avatar").then((module) => ({ default: module.Avatar }))
+  import("@nextui-org/avatar").then((module) => ({ default: module.Avatar })),
 );
 
 export default Avatar;
--- src/components/ui/MenuList.tsx
+++ src/components/ui/MenuList.tsx
@@ -3,11 +3,11 @@
 import { lazy } from "react";
 
 export const Listbox = lazy(() =>
-  import("@nextui-org/listbox").then((module) => ({ default: module.Listbox }))
+  import("@nextui-org/listbox").then((module) => ({ default: module.Listbox })),
 );
 
 export const ListboxItem = lazy(() =>
   import("@nextui-org/listbox").then((module) => ({
     default: module.ListboxItem,
-  }))
+  })),
 );
